e5407c13e50b023dfe48eac5b845a9b73a779191
Eduardo <eduardo212792@gmail.com>
2021-05-17 14:37:28 -0600

    Added Json class for json management

---
+++ src/util/Json.h
@@ -0,0 +1,14 @@
+//
+// Created by eduardozumbadog on 5/17/21.
+//
+
+#ifndef PROYECTO_II_SERVER_CE2103_JSON_H
+#define PROYECTO_II_SERVER_CE2103_JSON_H
+
+
+class Json {
+
+};
+
+
+#endif //PROYECTO_II_SERVER_CE2103_JSON_H
